3c12f33d3f73e88591da1590088d09127ce32afd
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-28 15:04:30 -0700

    Lab-4-8-Camerino A

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
-    return plist
-
-
+    begin = int(input(f'Enter 1st Value'))
+    end = int(input(f'Enter 2nd Value greater than 1st'))
+    for num in range(begin,end):
+        if num>1: 
+            for i in range(2,num):
+                if num%i ==0:
+                    break
+                else:
+                    plist.append(num)
+                    print(num)
 if __name__ == '__main__':
     main()
